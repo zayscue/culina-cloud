cc115138e41a5af209a731e6985b9f07ad352e3e
Zackery Ayscue <ztayscue@gmail.com>
2021-03-20 23:50:33 -0400

    Configured serilog and the log provider for the cook book service

--- src/Services/CookBook/CookBook.API/Program.cs
+++ src/Services/CookBook/CookBook.API/Program.cs
@@ -1,20 +1,68 @@
+using System;
+using System.IO;
+using Serilog;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Culina.CookBook.Application;
+using Culina.CookBook.Infrastructure;
 
-namespace Culina.CookBook.API
+IConfiguration GetConfiguration()
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+    return new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddJsonFile($"appsettings.{env}.json", optional: true, reloadOnChange: true)
+        .AddEnvironmentVariables()
+        .Build();
+}
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+var configuration = GetConfiguration();
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(configuration)
+    .CreateLogger();
+
+try
+{
+    Log.Information("Starting web host");
+    WebHost.CreateDefaultBuilder()
+        .ConfigureServices(services =>
+        {
+            services.AddApplication();
+            services.AddInfrastructure(configuration);
+        })
+        .Configure((WebHostBuilderContext webHostBuilderContext, IApplicationBuilder app) =>
+        {
+            var env = webHostBuilderContext.HostingEnvironment;
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseEndpoints(e =>
+            {
+                e.MapGet("/", async context =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    await context.Response.WriteAsync("Hello World!");
                 });
-    }
+            });
+        })
+        .Build()
+        .Run();
+    return 0;
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly");
+    return 1;
+}
+finally
+{
+    Log.CloseAndFlush();
 }
