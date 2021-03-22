d61d025abc98aa90c0980afa453bf2db59cb0783
Zackery Ayscue <ztayscue@gmail.com>
2021-03-22 06:24:18 -0400

    Quick fix.

---
+++ src/Services/CookBook/CookBook.Application/Ingredients/Queries/GetIngredients/GetIngredientsQueryValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Culina.CookBook.Application.Ingredients.Queries.GetIngredients
+{
+    public class GetIngredientsQueryValidator : AbstractValidator<GetIngredientsQuery>
+    {
+        public GetIngredientsQueryValidator()
+        {
+            RuleFor(x => x.Page)
+                .GreaterThanOrEqualTo(1).WithMessage("Page at least greater than or equal to 1.");
+
+            RuleFor(x => x.Limit)
+                .GreaterThanOrEqualTo(1).WithMessage("Limit at least greater than or equal to 1.");
+        }
+    }
+}
