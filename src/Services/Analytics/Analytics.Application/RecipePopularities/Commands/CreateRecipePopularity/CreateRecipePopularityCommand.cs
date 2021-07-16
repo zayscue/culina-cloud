b8659f48e832482ac1785b01e4e1deacf84e6aa5
Zackery Ayscue <ztayscue@gmail.com>
2021-07-16 12:55:13 -0400

    Started the recipe popularity create command.

---
+++ src/Services/Analytics/Analytics.Application/RecipePopularities/Commands/CreateRecipePopularity/CreateRecipePopularityCommand.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CulinaCloud.Analytics.Application.RecipePopularities.Commands.CreateRecipePopularity
+{
+    public class CreateRecipePopularityCommand : IRequest<CreateRecipePopularityResponse>
+    {
+        public Guid RecipeId { get; set; }
+        public string Submitted { get; set; }
+        public int RatingCount { get; set; }
+        public int RatingSum { get; set; }
+        public decimal RatingAverage { get; set; }
+        public decimal RatingWeightedAverage { get; set; }
+    }
+
+    public class CreateRecipePopularityCommandHandler : IRequestHandler<CreateRecipePopularityCommand, CreateRecipePopularityResponse>
+    {
+        public Task<CreateRecipePopularityResponse> Handle(CreateRecipePopularityCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Services/Analytics/Analytics.Application/RecipePopularities/Commands/CreateRecipePopularity/CreateRecipePopularityCommandValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+
+namespace CulinaCloud.Analytics.Application.RecipePopularities.Commands.CreateRecipePopularity
+{
+    public class CreateRecipePopularityCommandValidator : AbstractValidator<CreateRecipePopularityCommand>
+    {
+        public CreateRecipePopularityCommandValidator()
+        {
+            RuleFor(c => c.RecipeId)
+                .NotEmpty();
+
+            RuleFor(c => c.Submitted)
+                .NotEmpty();
+
+            RuleFor(c => c.RatingCount)
+                .GreaterThan(0);
+
+            RuleFor(c => c.RatingSum)
+                .GreaterThan(0);
+
+            RuleFor(c => c.RatingAverage)
+                .GreaterThan(0);
+
+            RuleFor(c => c.RatingWeightedAverage)
+                .GreaterThan(0);
+        }
+    }
+}
