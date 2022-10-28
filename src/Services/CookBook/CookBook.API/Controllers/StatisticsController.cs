93df05aaaf0b14b6e04a875e1120f2df4282b1cb
Zackery Ayscue <ztayscue@gmail.com>
2022-10-28 11:49:27 -0500

    Finished implementing CookBook statistics endpoint.

---
+++ src/Services/CookBook/CookBook.API/Controllers/StatisticsController.cs
@@ -0,0 +1,18 @@
+﻿using CulinaCloud.BuildingBlocks.API.Controllers;
+using CulinaCloud.CookBook.Application.CookBookStatistics.Queries.GetCookBookStatistics;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CulinaCloud.CookBook.API.Controllers
+{
+    [Route("statistics")]
+    public class StatisticsController : ApiControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<GetCookBookStatisticsResponse>> Get()
+        {
+            var response = await Mediator.Send(new GetCookBookStatisticsQuery());
+            return Ok(response);
+        }
+    }
+}
---
+++ src/Services/CookBook/CookBook.Domain/Entities/Statistics.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace CulinaCloud.CookBook.Domain.Entities
+{
+    public record Statistics
+    {
+        public RecipeStatistics RecipeStatistics { get; set; } = new();
+        public List<RecipeIngredientStatistic> IngredientStatistics { get; set; } = new();
+        public List<RecipeTagStatistic> TagStatistics { get; set; } = new();
+    }
+
+    public record RecipeStatistics
+    {
+        public int RecipeCount { get; set; }
+    }
+
+    public record RecipeIngredientStatistic
+    {
+        public int RecipeCount { get; set; }
+        public string IngredientName { get; set; }
+    }
+
+    public record RecipeTagStatistic
+    {
+        public int RecipeCount { get; set; }
+        public string TagName { get; set; }
+    }
+}
